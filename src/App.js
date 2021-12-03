815f69add2691de33c1e9e61c81e4165e693da88
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-03 10:44:04 -0500

    Weather & App CSS updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App Final</h1>
         <Weather />
         <footer>
           {" "}
