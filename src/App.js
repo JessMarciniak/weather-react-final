542e78039fd36b1d1ffb5b45c43906308584e909
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-03 08:41:11 -0500

    Added footer with code link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App Final</div>;
+  return (
+    <div className="App">
+      Weather App Final{" "}
+      <footer>
+        {" "}
+        This project was coded by Jessica Marciniak and is{" "}
+        <a href="https://github.com/JessMarciniak/weather-react-final">
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
