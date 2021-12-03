9a12c11eb907767a88a9384e89dfde36f5c8cb9f
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-03 09:41:46 -0500

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App Final{" "}
-      <footer>
-        {" "}
-        This project was coded by Jessica Marciniak and is{" "}
-        <a href="https://github.com/JessMarciniak/weather-react-final">
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App Final</h1>
+        <div className="location">
+          <button className="btn btn-primary">Testing</button>
+        </div>
+        <footer>
+          {" "}
+          This project was coded by Jessica Marciniak and is{" "}
+          <a href="https://github.com/JessMarciniak/weather-react-final">
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
