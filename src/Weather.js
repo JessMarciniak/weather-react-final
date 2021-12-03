53ff921aed33485eb99e4db7f3dfffd1ccc94beb
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-03 16:49:58 -0500

    Updated icon URL to match search

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       highTemp: response.data.main.temp_max,
       lowTemp: response.data.main.temp_min,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
   }
